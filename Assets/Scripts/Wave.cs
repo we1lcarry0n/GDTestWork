55dcd69242b26c899e1019eec01103370340997e
Oleg <blakbirld1@gmail.com>
2023-11-13 23:30:58 +0200

    Добавьте файлы проекта.

---
+++ Assets/Scripts/Wave.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Wave", menuName = "Data/Waves")]
+[Serializable]
+public class Wave : ScriptableObject
+{
+
+    public GameObject[] Characters;
+}
